5d97dcece8d10127e6e78f3c49072d0cc8db3315
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-01-28 16:32:14 -0500

    fixed pause function

--- javascript/script.js
+++ javascript/script.js
@@ -67,6 +67,7 @@ $(document.body).on("click", ".plane",function() {
             if (state === "still"){
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr("src", $(this).attr("animate"));
+                $(this).attr("data-state","animate")
             } else {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
